6443bea0d891f88737beca3338dc5713fd66025e
Jaime Aranzazu <jaedaraz@gmail.com>
2023-01-29 18:57:44 -0500

    Added calculation of seconds until next birthday

--- src/functions/add-birthday/index.ts
+++ src/functions/add-birthday/index.ts
@@ -5,17 +5,35 @@ import { addBirthday } from "@libs/dynamo";
 import { schema } from "./schema";
 
 const handlerLambda: ValidatedEventAPIGatewayProxyEvent = async (event) => {
-  const input = event.body;
+  const { birthday, name } = event.body;
+  const secondsToNextBirthday = getBirthdayTTL(birthday);
+
   await addBirthday({
-    id: input.birthday,
-    birthday: input.birthday,
-    name: input.name
+    id: birthday,
+    birthday: birthday,
+    name: name
   });
 
   return formatJSONResponse({
-    response: input,
+    response: event.body,
     statusCode: 200
   })
+}
+
+function getBirthdayTTL(birthday: string) {
+  let today = new Date();
+  let todayUTC = new Date(Date.UTC(today.getUTCFullYear(), today.getUTCMonth(), today.getUTCDate(),
+    today.getUTCHours(), today.getUTCMinutes()));
+
+  let birthdayArray = birthday.split("/").map(Number);
+  let nextBirthdayUTC = new Date(Date.UTC(todayUTC.getFullYear(), birthdayArray[1] - 1, birthdayArray[0], 12));
+
+  if (todayUTC >= nextBirthdayUTC) {
+    nextBirthdayUTC.setFullYear(todayUTC.getFullYear() + 1);
+  }
+
+  const secondsToNextBirthday = nextBirthdayUTC.getTime() / 1000 - todayUTC.getTime() / 1000;
+  return secondsToNextBirthday;
 }
 
 export const handler = middyfy(handlerLambda, schema);
