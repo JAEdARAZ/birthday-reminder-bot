b2fd594a1d5ce5ea20c58cac134888bfa952231a
Jaime Aranzazu <jaedaraz@gmail.com>
2023-01-29 15:59:54 -0500

    Added birthday date validation

--- src/functions/add-birthday/schema.ts
+++ src/functions/add-birthday/schema.ts
@@ -1,9 +1,13 @@
 import * as Joi from "joi";
+// 
 
 export const schema =
   Joi.object({
     body: Joi.object({
-      birthday: Joi.string().required(),
+      birthday: Joi.string()
+        .regex(/^(0?[1-9]|[12][0-9]|3[01])[\/](0?[1-9]|1[012])[\/]\d{4}$/)
+        .message("body.birthday invalid. Correct date format to: DD/MM/YYYY")
+        .required(),
       name: Joi.string().required()
     }).required()
   }).unknown(true)
